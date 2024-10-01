b78d22c1be5e627dd874a042ca46e763bfead4fe
Trang Huyen <caothihuyenttrang@gmail.com>
2024-10-01 19:31:58 +0700

    Account Settings

---
+++ BlogWebsite/Components/Account/Settings/UserSettingViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Client.Components.Account.Settings
+{
+    public class UserSettingViewModel
+    {
+        public Guid Id { get; set; }
+        public string Descript { get; set; }
+        public string Email { get; set; }   
+        public string Img { get; set; } 
+        public string PhoneNumber { get; set; }
+    }
+}
