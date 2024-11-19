0ca0d6d76c614a39522bc1c390a588d60ce4699f
Dang Giap <dgiap540@gmail.com>
2024-11-19 22:50:16 +0700

    get old login register

--- API/DTO/UserDto.cs
+++ API/DTO/UserDto.cs
@@ -7,10 +7,10 @@
         public string? FullName { get; set; }
         public string? ImgCover { get; set; }
         public string? Address { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public string Id { get; set; }
-        public string IdViewer { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? Id { get; set; }
+        public string? IdViewer { get; set; }
         public int CountFollow { get; set; }
         public bool IsFollowing { get; set; }
     }
